7125f162166b205e5945c36e87b8401607955136
Camilla Toso <39516759+Camilla223@users.noreply.github.com>
2025-05-14 09:19:42 +0000

    basic components created

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,23 +1,25 @@
 const emojipedia = [
   {
     id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "🥲",
+    name: "	Smiling face with tear",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "The smiling face with tear emoji stands for smiling through the tough times and symbolizes trying to stay positive even in difficult moments. You are smiling through the pain instead of being truly happy.",
   },
   {
     id: 2,
     emoji: "🙏",
     name: "Person With Folded Hands",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
   },
   {
     id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+      "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things.",
+  },
 ];
+
+export default emojipedia;
